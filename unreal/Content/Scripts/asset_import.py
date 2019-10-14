6c57ef32920ac74d941830a88b67c93f501816be
Sam <serious.sam.2008@gmail.com>
2019-10-14 23:43:09 +0800

    Daily

--- unreal/Content/Scripts/asset_import.py
+++ unreal/Content/Scripts/asset_import.py
@@ -2,24 +2,23 @@ import unreal_engine as ue
 
 
 def import_asset(stage, source, target, skeleton=None):
-    from unreal_engine.classes import PyFbxFactory, Skeleton
 
-    ue.log('stage:' + stage)
-    ue.log('source:' + source)
-    ue.log('target:' + target)
-    ue.log('skeleton:' + skeleton)
+    from unreal_engine.classes import PyFbxFactory, Skeleton
+    from unreal_engine.enums import EFBXImportType
 
     target = target[:-1] if target[-1] in ['\\', '/'] else target
 
     factory = PyFbxFactory()
     factory.ImportUI.bCreatePhysicsAsset = False
     factory.ImportUI.bImportMaterials = True if stage in ['mdl'] else False
-    # factory.ImportUI.bImportAsSkeletal = True if stage in ['skn', 'rig'] else False
     factory.ImportUI.bImportAnimations = True if stage in ['blk', 'anm'] else False
+
+    if stage in ['blk', 'anm']:
+        factory.ImportUI.MeshTypeToImport = EFBXImportType.FBXIT_Animation
 
     skel_assets = ue.get_assets_by_class('Skeleton')
     for skel in skel_assets:
-        if skel.get_name() == skeleton:
+        if stage in ['blk', 'anm'] and skeleton and skel.get_name().count(skeleton):
             factory.ImportUI.skeleton = skel
             break
 
